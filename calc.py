c1354aeffc47c85f329ef78cf9eb91dacd356d13
carlosgarijo <carlos.garijo.gyg@gmail.com>
2015-09-21 10:52:27 +0200

    Añadida la suma

--- calc.py
+++ calc.py
@@ -3,8 +3,22 @@
 
 # Vamos a programar una calculadora
 
+import sys
+
 def main():
     pass
 
+def suma(sumando1, sumando2):
+    return sumando1 + sumando2
+
+
 if __name__ == "__main__":
-    main()
+    #print (sys.argv)
+    try:
+        operando1 = int(sys.argv[2])
+        operando2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Tienen que ser enteros")
+        
+    print(suma(operando1, operando2))
+    #main()
